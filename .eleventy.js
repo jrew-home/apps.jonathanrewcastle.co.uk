fe749566b715dd33a0b767b516da7a61a099c31e
Jonathan Rewcastle <j.rewcastle1@gmail.com>
2019-11-20 20:36:12 +0000

    Sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("sitemap.xml");
   return { dir: { output: 'docs' } };
 };
