f717288722d287a8b555495c559b778c9b40166e
Jonathan Rewcastle <j.rewcastle1@gmail.com>
2019-11-21 18:54:22 +0000

    Improvements to support email

---
+++ docs/assets/jra.js
@@ -0,0 +1,14 @@
+window.supportEmail = () => {
+  var keys = {};
+  window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(a,name,value){keys[name]=value;});
+  var subject = 'Support Query';
+  if (keys.app) {
+    var app = keys.app.replace('-', ' ');
+    subject += ': ' + app;
+  }
+  if (keys.platform) {
+    subject += ' (' + keys.platform + ')';
+  }
+  window.location = 'mailto://jrewapps@gmail.com?subject=' + subject;
+  console.log(subject);
+}
