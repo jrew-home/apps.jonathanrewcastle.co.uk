c7b18547557ecae48757740109f766b3d7adc729
Jonathan Rewcastle <j.rewcastle1@gmail.com>
2020-04-07 19:02:23 +0100

    Update site to use Jekyll

--- .eleventy.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addPassthroughCopy("sitemap.xml");
-  return { dir: { output: 'docs' } };
-};
